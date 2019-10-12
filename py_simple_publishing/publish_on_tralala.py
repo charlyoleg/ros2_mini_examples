2a4abcd881fb046aca21da21e2175b7339e13113
charli <charlyoleg@fabfolk.com>
2019-10-12 17:48:53 +0200

    reformating according to .editorconfig

--- py_simple_publishing/publish_on_tralala.py
+++ py_simple_publishing/publish_on_tralala.py
@@ -8,35 +8,36 @@ from std_msgs.msg import String
 
 class PublishOnTralala(Node):
 
-    def __init__(self):
-        #super().__init__('simple_pub') # you can choose any name for the node
-        super().__init__('publish_on_tralala') # but, it is easier that the ros-node-name is identical to the python-module-name
-        self.publisher_ = self.create_publisher(String, 'tralala', 10)
-        timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+  def __init__(self):
+    #super().__init__('simple_pub') # you can choose any name for the node
+    super().__init__('publish_on_tralala') # but, it is easier that the ros-node-name is identical to the python-module-name
+    self.publisher_ = self.create_publisher(String, 'tralala', 10)
+    timer_period = 0.5  # seconds
+    self.timer = self.create_timer(timer_period, self.timer_callback)
+    self.i = 0
 
-    def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
+  def timer_callback(self):
+    msg = String()
+    msg.data = 'Hello World: %d' % self.i
+    self.publisher_.publish(msg)
+    self.get_logger().info('Publishing: "%s"' % msg.data)
+    self.i += 1
 
 
 def main(args=None):
-    rclpy.init(args=args)
+  rclpy.init(args=args)
 
-    the_node = PublishOnTralala()
+  the_node = PublishOnTralala()
 
-    rclpy.spin(the_node)
+  rclpy.spin(the_node)
 
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    the_node.destroy_node()
-    rclpy.shutdown()
+  # Destroy the node explicitly
+  # (optional - otherwise it will be done automatically
+  # when the garbage collector destroys the node object)
+  the_node.destroy_node()
+  rclpy.shutdown()
 
 
 if __name__ == '__main__':
-    main()
+  main()
+
