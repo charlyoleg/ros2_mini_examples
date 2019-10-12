8053ab64be16f7b4cc078a693bbf6dfa9baf16f4
charli <charlyoleg@fabfolk.com>
2019-10-12 17:01:09 +0200

    add a simple py_publisher

---
+++ py_simple_publishing/publish_on_tralala.py
@@ -0,0 +1,41 @@
+# publish_on_tralala.py
+
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String
+
+
+class PublishOnTralala(Node):
+
+    def __init__(self):
+        super().__init__('simple_pub')
+        self.publisher_ = self.create_publisher(String, 'tralala', 10)
+        timer_period = 0.5  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = 'Hello World: %d' % self.i
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.i += 1
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    the_node = PublishOnTralala()
+
+    rclpy.spin(the_node)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    the_node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
---
+++ py_simple_publishing/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+package_name = 'py_simple_publishing'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    py_modules=[
+        'publish_on_tralala',
+    ],
+    data_files=[
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='charlyoleg',
+    author_email='charlyoleg@fabfolk.com',
+    maintainer='charlyoleg',
+    maintainer_email='charlyoleg@fabfolk.com',
+    keywords=['ROS2'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description='A mini publisher using rclpy.',
+    license='Apache License, Version 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'publish_on_tralala = publish_on_tralala:main',
+        ],
+    },
+)
+
